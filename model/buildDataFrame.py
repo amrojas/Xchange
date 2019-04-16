9f0fed2094978e9aeb851eff4d7159fde0c9e9b2
Gabe Villasana <villagab4@gmail.com>
2019-04-16 11:01:36 -0400

    Need to complete function for building dataframe

--- model/buildDataFrame.py
+++ model/buildDataFrame.py
@@ -1,16 +1,17 @@
 '''
+menu_items is a list of the items on the menu
 average_temp is a list containing the average temperature for each day in the range of dates
-first_day_of_week is an int of the first day of order (Monday = 1, Tuesday = 2 ... Sunday = 7)
-first_week_number is an int of the first week of order (First week of January = 1, ... Last week of December = 52)
+days_of_week is a list of all the days for the order
+week_numbers is a list of all the weeks for the order
 last_day_week and last_week_number are analogous
 weather is a list containing the weather conditions in order (Ex: ["Sun", "Rain", "Snow", "Rain", "Sun"])
 '''
-def build_new_data(average_temp, first_day_of_week, first_week_number, last_day_of_week, last_week_number, weather):
+def build_new_data(menu_item, average_temp, days_of_week, week_numbers , weather):
 
     new_data = {}
     new_data["Average Temperature"] = average_temp
-    new_data["Day of Week"] = []
-    new_data["Week Number"] = []
+    new_data["Day of Week"] = days_of_week
+    new_data["Week Number"] = week_numbers
     new_data["Sun"] = [1 if w == "Sun" else 0 for w in weather]
     new_data["Rain"] = [1 if w == "Rain" else 0 for w in weather]
     new_data["Snow"] = [1 if w == "Snow" else 0 for w in weather]
